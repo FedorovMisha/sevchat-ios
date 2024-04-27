6e149e37350a74c369172fd50ce93935388b5f5e
Misha Fedorov <frvmi@MacBook-Pro-Misha.local>
2024-04-27 12:03:16 +0300

    Initial project

---
+++ sevchat-app/sevchat-app/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  sevchat-app
+//
+//  Created by Misha Fedorov on 26.04.2024.
+//
+
+import SwiftUI
+import DaVinci
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+            Text("Hello, world!")
+
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sevchat-core/Sources/Aesthetic/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  
+//
+//  Created by Misha Fedorov on 26.04.2024.
+//
+
+import DaVinci
+
